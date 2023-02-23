668c4a76c2a369194da0f36635e5a1b9a714a3c0
Tuanhung2911 <tthhn2003@gmail.com>
2023-02-23 11:01:19 +0700

    Bài Tập

---
+++ TranTuanHung_Project/src/HocJava/TranTuanHung_App.java
@@ -0,0 +1,9 @@
+package HocJava;
+
+public class TranTuanHung_App {
+
+	public static void main(String[] args) {
+		System.out.println("Trần Tuấn Hưng học Java");
+	}
+
+}
---
+++ TranTuanHung_Project/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Hung
+ *
+ */
+module TranTuanHung_Project {
+}
